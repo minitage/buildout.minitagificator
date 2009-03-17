8aebd3af5e5c761316172a43f4714f156e211815
kiorky user <kiorky@judith.(none)>
2009-03-17 01:32:59 +0100

    packaging

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import os
 
-version = '1.0'
+version = '1.1'
 
 entry_point = 'buildout.minitagificator.minitagificator:install'
 entry_points = {"zc.buildout.extension": ["default = %s" % entry_point]} 
@@ -23,8 +23,8 @@ setup(name='buildout.minitagificator',
       author_email='kiorky@cryptelium.net',
       url='http://pypi.python.org/pypi/buildout.minitagificator',
       license='GPL',
-      packages = find_packages('src'),
       package_dir = {'': 'src'},
+      packages = find_packages('src'),
       namespace_packages=['buildout', 'buildout.minitagificator'],
       include_package_data=True,
       zip_safe=False,
