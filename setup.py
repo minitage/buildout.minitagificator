461e1048b1b70b2ab3b85b990627a1df7b7e8f10
Mathieu Le Marec - Pasquet <kiorky@cryptelium.net>
2012-06-05 09:07:31 +0200

    Preparing release 2.4.18

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import os
 
-version = '2.4.18dev'
+version = '2.4.18'
 
 entry_point = 'buildout.minitagificator.minitagificator:install'
 entry_points = {"zc.buildout.extension": ["default = %s" % entry_point]}
